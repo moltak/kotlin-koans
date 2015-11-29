b5e381a5947839907a803a252c35544dde011763
moltak <moltak@gmail.com>
2015-11-29 16:48:23 +0900

    .

---
+++ src/i_introduction/_8_Smart_Casts/JavaCode6.kt
@@ -0,0 +1,12 @@
+package i_introduction._8_Smart_Casts
+
+/**
+ * Created by engeng on 11/27/15.
+ */
+class JavaCode6 {
+    fun eval(expr: Expr) : Int {
+        when(expr) {
+            case
+        }
+    }
+}
